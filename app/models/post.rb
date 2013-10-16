3b95a6e5c5d2c85d1c6e194af79c08b959cb5d65
Gurur Sarbanoglu <thepurpletwilight@yahoo.co.uk>
2013-10-16 22:16:12 +0100

    Add user following

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,11 @@ class Post < ActiveRecord::Base
 	validates :content, presence: true
 	validates :title, presence: true
 	validates :user_id, presence: true
+
+	def self.from_users_followed_by(user)
+    followed_user_ids = "SELECT followed_id FROM relationships
+                         WHERE follower_id = :user_id"
+    where("user_id IN (#{followed_user_ids}) OR user_id = :user_id",
+          user_id: user.id)
+	end
 end
