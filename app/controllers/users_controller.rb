84d1c6d0fbdb4cfdee0d6463288d189f987a4456
Gurur Sarbanoglu <thepurpletwilight@yahoo.co.uk>
2013-10-16 22:49:54 +0100

    Went through chapter 11

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,14 +48,14 @@ class UsersController < ApplicationController
   end
 
   def following
-    @title = "Following"
+    @head = "Following"
     @user = User.find(params[:id])
     @users = @user.followed_users.paginate(page: params[:page])
     render 'show_follow'
   end
 
   def followers
-    @title = "Followers"
+    @head = "Followers"
     @user = User.find(params[:id])
     @users = @user.followers.paginate(page: params[:page])
     render 'show_follow'
